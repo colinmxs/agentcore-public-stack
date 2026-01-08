9e567ea8b636207a89c0e714e918b2b39c7d38d9
derrickfink <derrickfink@boisestate.edu>
2026-01-08 16:10:37 -0700

    Update development environment configuration to enable production features
    - Changed the production flag to false for local development.
    - Updated appApiUrl and inferenceApiUrl to point to the beta API endpoint and AWS Bedrock service, respectively.
    - Enabled authentication for local development to align with production settings.

--- frontend/ai.client/src/environments/environment.development.ts
+++ frontend/ai.client/src/environments/environment.development.ts
@@ -1,11 +1,6 @@
 export const environment = {
-    production: true,
-    appApiUrl: 'http://localhost:8000',
-    inferenceApiUrl: 'http://localhost:8001',
-    enableAuthentication: false,  // Disabled for local development
-
-    production1: false,
-    appApiUrl1: 'https://api.beta.boisestate.ai',
-    inferenceApiUrl1: 'https://bedrock-agentcore.us-west-2.amazonaws.com/runtimes/arn:aws:bedrock-agentcore:us-west-2:490617140655:runtime/bsu_agentcore_agentcore_runtime-2NEHng8HmA',
-    enableAuthentication1: true
+    production: false,
+    appApiUrl: 'https://api.beta.boisestate.ai',
+    inferenceApiUrl: 'https://bedrock-agentcore.us-west-2.amazonaws.com/runtimes/arn:aws:bedrock-agentcore:us-west-2:490617140655:runtime/bsu_agentcore_agentcore_runtime-2NEHng8HmA',
+    enableAuthentication: true
 };
